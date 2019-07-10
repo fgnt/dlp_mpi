97f4550797c8f387b36178cbec435862d8eb05cf
Lukas Drude <mail@lukas-drude.de>
2019-07-10 20:48:34 +0200

    Improve MPI doc

--- examples/mpi_5_map_unordered.py
+++ examples/mpi_5_map_unordered.py
@@ -1,9 +1,17 @@
+"""
+Run on single CPU:
+python paderbox/examples/mpi_5_map_unordered.py
+
+Run on multiple CPUs:
+mpiexec -np 5 python paderbox/examples/mpi_5_map_unordered.py
+"""
+
 from paderbox.utils.mpi import COMM, RANK, SIZE, MASTER, IS_MASTER, map_unordered
 import time
 import numpy as np
 
 
-def function(example_id):
+def fn(example_id):
     time.sleep(np.random.uniform(0, 1))
     example = 'hello'
     print(RANK, example_id, example[example_id])
@@ -14,11 +22,11 @@ if __name__ == '__main__':
     if IS_MASTER:
         print('### Unordered map scattered around processes:')
 
-    result = list(map_unordered(function, range(5)))
+    result = list(map_unordered(fn, range(5)))
 
     if IS_MASTER:
         print(result)
 
     if IS_MASTER:
         print('### Map function run only on master:')
-        print(list(map(function, range(5))))
+        print(list(map(fn, range(5))))
