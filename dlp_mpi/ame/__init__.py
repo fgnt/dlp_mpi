407f91480c7f74fd20eaaf67c489688f6ae343e3
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2025-07-17 14:48:22 +0200

    clean up

--- dlp_mpi/ame/__init__.py
+++ dlp_mpi/ame/__init__.py
@@ -1,18 +1,3 @@
-"""
-AME: async based mpi like framework for embarrassingly parallel workloads in pure python
-ame: A mpi4py like framework for embarrassingly parallel workloads in pure python
-
-mpys: mpi like framework for embarrassingly parallel workloads in pure python implemented with sockets
-
-msp (
-
-smp: simple mpi for embarrassingly parallel
-
-sockmpi: socket based mpi like framework for embarrassingly parallel workloads in pure python
-
-"""
-import sys
-
 # Mirroring the behaviour of mpi4py, MPI has to be explicitly be imported
 # from . import MPI
 
